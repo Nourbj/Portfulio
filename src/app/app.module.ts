c299abb38b48ff5ae8a4fbae89df317da8551b24
NourBj <nbenjannena@gmail.com>
2025-10-27 15:02:59 +0100

    contact form with emailJS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -27,7 +27,8 @@ import { ProjectsComponent } from './components/projects/projects.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-contact',
@@ -6,20 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
-  formData = {
-    name: '',
-    email: '',
-    subject: '',
-    message: ''
-  };
+  formData = { name: '', email: '', subject: '', message: '' };
 
-  onSubmit() {
-    console.log('Form submitted:', this.formData);
-    this.formData = {
-      name: '',
-      email: '',
-      subject: '',
-      message: ''
+  serviceID = environment.EMAILJS_SERVICE_ID;
+  templateID = environment.EMAILJS_TEMPLATE_ID;
+  publicKey = environment.EMAILJS_PUBLIC_KEY;
+
+  sendMessage() {
+    const templateParams = {
+      user_name: this.formData.name,
+      user_email: this.formData.email,
+      subject: this.formData.subject,
+      message: this.formData.message
     };
+
+    emailjs.send(this.serviceID, this.templateID, templateParams, this.publicKey)
+      .then((response: EmailJSResponseStatus) => {
+        console.log('SUCCESS!', response.status, response.text);
+        alert('Email sent successfully!');
+        this.formData = { name: '', email: '', subject: '', message: '' };
+      }, (error) => {
+        console.error('FAILED...', error);
+        alert('Failed to send email. Please try again.');
+      });
   }
 }
